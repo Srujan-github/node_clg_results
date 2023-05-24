911c78786824360568971fe63d61e561de900559
Srujan-github <101975044+Srujan-github@users.noreply.github.com>
2023-05-24 15:22:39 +0530

    get request

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejs = require('ejs');
+const pdf = require('html-pdf');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
@@ -19,11 +21,11 @@ app.get('/', (req, res) => {
   // Render the HTML template and pass the data
   res.render('index', {  bol,hallNo,tab1,  all_gpa, all_res });
 });
-app.post('/re', async(req, res) => {
+app.get('/result', async(req, res) => {
   const url="https://github.com/Srujan-github/node_clg_results";
 try{
   // res.render('loading');
-    const hallNo = req.body.hallNo;
+    const hallNo = req.query.hallNo;
     const response = await fetch(`https://results-restapi.up.railway.app/all-r18/${hallNo}`);
     const result = await response.json();
   
@@ -56,6 +58,35 @@ try{
     res.status(400).render('404');
 }}
   );
+
+  app.get('/download', (req, res) => {
+    ejs.renderFile( path.join(__dirname, 'views', 'index.ejs'),{bol,tab1,all_gpa,all_res }, (error, renderedHtml) => {
+      if (error) {
+        console.log(error)
+        res.status(500).send('Error rendering EJS template');
+      } else {
+        const options = {
+          format:'A4',
+          orientation: 'portrait',
+        };
+  
+        pdf.create(renderedHtml, options).toFile((error, buffer) => {
+          if (error) {
+            console.log(error)
+            res.status(500).send('Error generating PDF');
+          } else {
+            res.set({
+              'Content-Type': 'application/pdf',
+              'Content-Disposition': 'attachment; filename="example.pdf"',
+              'Content-Length': buffer.length,
+            });
+  
+            res.send(buffer);
+          }
+        });
+      }
+    });
+  });
 // app.get("/test",(req,res)=>{
 //   res.render('test')
 // })
